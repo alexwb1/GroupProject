912cec1a1b308c2db36916f8ff9706c471d5587e
Mitchell Hammack <hammackm1@gmail.com>
2018-04-16 21:35:06 -0400

    Added StockData header

---
+++ src/StockData.h
@@ -0,0 +1,33 @@
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*
+
+This  class will  hold an object for  each finiacal  instrument. The data will be
+stored in a vector. Each index of the  vector represents one day of closing price.
+
+The constructor of the  class will take in the symbol or name of the instrument as
+it is in the "stock_data.txt" data file and read that data into the "data" vector.
+
+*/
+
+class InstrumentData
+{
+	vector<int> data; // vector to hold each days' data. 
+	string symbol;    // string to hold the symbol of the stock
+	string fileName;  // string to hold the name of the file that the data is being access from
+	ifstream inFile;  // file object
+	string inter;     // string used to tokenize the file lines
+	
+	
+	public:
+		StockData(string);
+		string getSymbol();
+		int getDayData(int dayNumber);
+		
+}
