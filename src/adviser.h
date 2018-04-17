e9f1fa1ae878a0dcb814e6e2aae690bdb3250c7d
Mitchell Hammack <hammackm1@gmail.com>
2018-04-16 23:10:38 -0400

    Adviser Changes

--- src/adviser.h
+++ src/adviser.h
@@ -20,16 +20,18 @@ class Adviser
     unsigned double fee; // a double value that represents the daily fee charged by the adviser
 
     unsigned int accuracy; // an int value (1 - 10) that represents the acurracy of the adviser's predictions
+	
+private:
+
+	void generateAccuracy(); // generates a value (1 - 10) representing the accuracy of an adviser's predictions
+	
+	void generateFee(); // generates a value representing the daily fee charged by the adviser
 
 public:
 
     Adviser(string name);
 
     double getFee(); // returns a value representing the daily fee charge by the adviser
-
-    void generateFee(); // generates a value representing the daily fee charged by the adviser
-
-    void generateAccuracy(); // generates a value (1 - 10) representing the accuracy of an adviser's predictions
 
     string generateAdvice(); // generates advice on asset movements for the current day
 };
