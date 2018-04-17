330410270d59e1abb57c5403d2197a2995cd4b87
Isaiah Nields <imnields@gmail.com>
2018-04-17 16:12:44 -0400

    Layout Added

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,31 @@ void promptAssets()
 
 int main()
 {
+    //welcomeMessage()
+
+    //promptBrokerage()
+
+    //promptAdviser()
+
+    //promptAssets()
+
+
+    //Loop through 6 months done weekly (24 weeks)
+    //bankDecisions()
+    //is a switch statement with calls to other methods
+    //checkAccountInfo()
+    //
+    //checkInvestment()
+    //prints all investments
+    //modifyInvestment()
+    //add()
+    //sell()
+    //getAdvice()
+    //nextWeek()
+
+    //explainHowDayWent();
+    //suggestionForNextDay();
+
 	std::cout << "test run" << std::endl;
 	getchar();
 	return 0;
