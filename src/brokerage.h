6f218cbacc9edc43d3d7199f5b3f57bb3707c920
Isaiah Nields <imnields@gmail.com>
2018-04-16 22:56:10 -0400

    Brokerage Update

--- src/brokerage.h
+++ src/brokerage.h
@@ -5,8 +5,17 @@
 
 class Brokerage
 {
-    string name;
-    vector<Adviser> advisers;
+    string name; // a string value that represents the name of the brokerage
+
+    double fee; // a double value that represents the fee per trade charged by the brokerage
+
+    vector<Adviser> advisers; // a list of advisers that are part of the brokerage
+
+public:
+
+    double getFee(); 
+
+    double generateFee(); // generates a double value that represents the fee per trade charged by the brokerage
 
 };
 
